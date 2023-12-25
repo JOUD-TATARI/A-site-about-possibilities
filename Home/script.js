007b89e464632b0050386154a1217eb117190a7f
JOUD-TATARI <149987332+JOUD-TATARI@users.noreply.github.com>
2023-12-25 20:25:09 +0300

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".icons-father").onclick=function()
+{
+    document.querySelector(".list-icons-father").style.display="block"
+        document.querySelector(".list-icons-father").style.transition="all 200ms ease"
+
+
+}
+document.querySelector(".cancel").onclick=function()
+{
+    document.querySelector(".list-icons-father").style.display="none"
+
+}
---
+++ content_test/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".icons-father").onclick=function()
+{
+    document.querySelector(".list-icons-father").style.display="block"
+        // document.querySelector(".list-icons-father").style.transition="all 0.5s ease-in"
+
+
+}
+document.querySelector(".cancel").onclick=function()
+{
+    document.querySelector(".list-icons-father").style.display="none"
+
+}
